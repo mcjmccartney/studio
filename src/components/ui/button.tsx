fe9f6e74049f6b8ebdb934e8b3c12911ab0449dc
Matthew McCartney <mcjmccartney@gmail.com>
2025-05-16 16:29:36 +0000

    Make the hover over the sidebar and all buttons this colour #9c4a38

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground hover:bg-[#9c4a38]",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
